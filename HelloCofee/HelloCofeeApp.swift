33ea5addb0fe5aec217855fa45b4910280e2fd9b
Anup Saud <anupsaud10@gmail.com>
2024-08-22 18:49:41 -0400

    Initial Commit

---
+++ HelloCofee/HelloCofeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCofeeApp.swift
+//  HelloCofee
+//
+//  Created by Anup Saud on 2024-08-22.
+//
+
+import SwiftUI
+
+@main
+struct HelloCofeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
